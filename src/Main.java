3052c3e6d6efe3f9b05a9eefe02d7703836105c5
Nikita Pantsevich <pantsevich15@yandex.ru>
2022-11-09 19:38:06 +0300

    system of summary

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Qwert");
+        int a = 0b1101010110;
+        int b = 012314;
+        int c = 456;
+        int d = 0x141D12;
+        System.out.println("Число a в двоичной системе: " + a);
+        System.out.println("Число b в восьмеричной системе: " + b);
+        System.out.println("Число c в десятичной системе: " + c);
+        System.out.println("Число d в шеснадцатиричной системе: " + d);
     }
 }
